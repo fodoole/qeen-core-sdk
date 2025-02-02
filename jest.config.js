63c3b15773d427302021eb3b66b034157bda33df
Omar Talafhah <162091392+omartalafhah@users.noreply.github.com>
2025-02-02 13:26:24 +0300

    Refactor test workflow and add Jest Puppeteer configuration

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,23 @@
 module.exports = {
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.mjs$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/',
+  projects: [
+    {
+      displayName: 'js-tests',
+      preset: 'jest-puppeteer',
+      testMatch: ['**/?(*.)+(test).js?(x)'],
+      moduleDirectories: ['node_modules', 'frontend-serving'],
+      moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
+    },
+    {
+      displayName: 'ts-tests',
+      transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.jsx?$': 'babel-jest',
+        '^.+\\.mjs$': 'babel-jest',
+      },
+      transformIgnorePatterns: ['/node_modules/'],
+      moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node', 'mjs'],
+      testEnvironment: 'node',
+      testMatch: ['**/?(*.)+(test).ts?(x)'],
+    },
   ],
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node', 'mjs'],
-  testEnvironment: 'node',
 };
